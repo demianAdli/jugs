06f06beba7e66b50d183988337e7c28f1790812b
Alireza Adli <alireza.adli@mail.concordia.ca>
2025-09-23 13:47:15 -0400

    Add resources/emissions and Update the app.py

---
+++ services/jug_ee/jugs_ee/resources/emissions.py
@@ -0,0 +1,27 @@
+from flask import request, jsonify
+from flask.views import MethodView
+from flask_smorest import Blueprint, abort
+
+from schemas import LCAInputDataSchema
+
+from lca_carbon_workflow import LCACarbonWorkflow
+
+blp = Blueprint('Emissions', 
+                __name__, 
+                description='Exporting embodied and end-of-life ' \
+                'emissions data for buildings')
+
+
+
+@blp.route("/emissions")
+class Emissions(MethodView):
+    @blp.arguments(LCAInputDataSchema)
+    def post(self, request_city):
+        try:
+            emissions_data = LCACarbonWorkflow(request_city,
+                                               'nrcan_archetypes.json', 
+                                               'nrcan_constructions_cap_3.json'
+                                               ).export_emissions()
+            return jsonify(emissions_data), 201
+        except Exception as e:
+            abort(500, message=str(e))
