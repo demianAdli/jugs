292147c6d1193a3b9284c26f206f12c3454a93c2
Alireza Adli <alireza.adli@mail.concordia.ca>
2025-09-23 13:19:16 -0400

    Add input geojson content

---
+++ services/jug_ee/jugs_ee/life_cycle_assessment/input_geojson_content.py
@@ -0,0 +1,34 @@
+"""
+JUGS project
+jug_ee project
+jugs_ee package
+input_geojson_content module
+Returns a temporary path to input the GeometryFactory
+Project developer: Alireza Adli alireza.adli4@gmail.com
+"""
+import tempfile
+import json
+
+
+class InputGeoJsonContent:
+  def __init__(self, content):
+    self.content = content
+
+  @property
+  def content(self):
+    return self._content
+
+  @content.setter
+  def content(self, content):
+    if isinstance(content, str):
+      self._content = content
+    else:
+      temp_file = \
+        tempfile.NamedTemporaryFile(
+          delete=False, suffix='.geojson', mode='w', encoding='utf8')
+      try:
+        json.dump(content, temp_file)
+        temp_file.flush()
+        self._content = temp_file.name
+      finally:
+        temp_file.close()
