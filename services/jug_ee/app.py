5bac05b98fb60d1414579567cd1861deca71e205
Alireza Adli <alireza.adli@mail.concordia.ca>
2025-09-26 16:33:28 -0400

    Edit to run the logging system

--- services/jug_ee/app.py
+++ services/jug_ee/app.py
@@ -1,7 +1,15 @@
-from flask import Flask
+import logging
+import secrets
+from time import perf_counter
+
+from flask import Flask, request, g
 from flask_smorest import Api
 
-from jug_ee.resources.emissions import blp as EmissionsBlueprint
+from jug_ee.resources.emissions import blp as emissions_blueprint
+from jug_chassis.logging.config import configure_logging, set_request_id, get_request_id
+
+configure_logging()
+logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
 
@@ -15,4 +23,40 @@ app.config['OPENAPI_SWAGGER_UI_URL'] = 'https://cdn.jsdelivr.net/npm/swagger-ui-
 
 api = Api(app)
 
-api.register_blueprint(EmissionsBlueprint)
+api.register_blueprint(emissions_blueprint)
+
+
+# ---- Correlation ID + access logging ----
+@app.before_request
+def _before():
+    rid = (request.headers.get("X-Request-ID")
+           or request.headers.get("X-Correlation-ID")
+           or secrets.token_hex(8))
+    set_request_id(rid)
+    g._t0 = perf_counter()
+
+
+@app.after_request
+def _after(resp):
+    try:
+        dur_ms = int((perf_counter() - getattr(g, "_t0", perf_counter())) * 1000)
+        client_ip = request.headers.get("X-Forwarded-For", request.remote_addr)
+        logger.info(
+            "http_request",
+            extra={
+                "method": request.method,
+                "path": request.path,
+                "status": resp.status_code,
+                "latency_ms": dur_ms,
+                "client_ip": client_ip,
+            },
+        )
+        resp.headers["X-Request-ID"] = get_request_id()
+    finally:
+        return resp
+
+
+@app.errorhandler(Exception)
+def _unhandled(e):
+    logger.exception("unhandled_exception")
+    return {"message": "Internal Server Error"}, 500
--- services/jug_ee/jug_ee/resources/emissions.py
+++ services/jug_ee/jug_ee/resources/emissions.py
@@ -1,27 +1,42 @@
-from flask import request, jsonify
+import os
+import logging
+
+from flask import request, jsonify, current_app
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
+from werkzeug.exceptions import HTTPException
 
 from jug_ee.schemas.schemas import LCAInputDataSchema
-
 from jug_ee.lca_carbon_workflow import LCACarbonWorkflow
 
-blp = Blueprint('Emissions', 
-                __name__, 
-                description='Exporting embodied and end-of-life ' \
-                'emissions data for buildings')
+logger = logging.getLogger(__name__)
+DEV_MODE = os.getenv("LOG_ENV", "dev") == "dev"
 
-
+blp = Blueprint(
+    'Emissions',
+    __name__,
+    description='Exporting embodied and end-of-life emissions data for buildings'
+)
 
 @blp.route("/emissions")
 class Emissions(MethodView):
     @blp.arguments(LCAInputDataSchema)
     def post(self, request_city):
+        logger.info("emissions_request_received")
         try:
-            emissions_data = LCACarbonWorkflow(request_city,
-                                               'nrcan_archetypes.json', 
-                                               'nrcan_constructions_cap_3.json'
-                                               ).export_emissions()
+            emissions_data = LCACarbonWorkflow(
+                request_city,
+                'nrcan_archetypes.json',
+                'nrcan_constructions_cap_3.json'
+            ).export_emissions()
+            logger.info("emissions_request_succeeded", extra={"buildings": len(emissions_data)})
             return jsonify(emissions_data), 201
+
+        except HTTPException:
+            # If something upstream already called abort(...), preserve that response.
+            raise
+
         except Exception as e:
-            abort(500, message=str(e))
+            logger.exception("emissions_request_failed", extra={"feature_count": feature_count})
+            public_msg = str(e) if (DEV_MODE or current_app.debug) else "Failed to compute emissions"
+            abort(500, message=public_msg)
