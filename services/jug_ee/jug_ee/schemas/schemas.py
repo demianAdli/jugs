f18ddd60206ff4cbc29a31c7cc51b6d7750c453e
Alireza Adli <alireza.adli@mail.concordia.ca>
2025-09-26 12:23:47 -0400

    Add schemas and init.py

---
+++ services/jug_ee/jug_ee/schemas/schemas.py
@@ -0,0 +1,36 @@
+"""
+schemas module
+Defines the schemas to post on emissions.py
+Developer: Alireza Adli alireza.adli4@gmail.com
+"""
+from marshmallow import Schema, fields
+
+
+class GeometrySchema(Schema):
+    """Schema for the geometry of a feature."""
+    type = fields.String(required=True)
+    coordinates = fields.List(
+        fields.List(fields.List(fields.Float())), required=True)
+
+
+class PropertiesSchema(Schema):
+    """Schema for the properties of a feature."""
+    name = fields.String(required=True)
+    address = fields.String(required=True)
+    function = fields.String(required=True)
+    height = fields.Float(required=True)
+    year_of_construction = fields.Integer(required=True)
+
+
+class FeatureSchema(Schema):
+    """Schema for a GeoJSON feature."""
+    type = fields.String(required=True)
+    geometry = fields.Nested(GeometrySchema, required=True)
+    id = fields.Integer(required=True)
+    properties = fields.Nested(PropertiesSchema, required=True)
+
+
+class LCAInputDataSchema(Schema):
+    """Define variables that come from user input."""
+    type = fields.String(required=True)
+    features = fields.List(fields.Nested(FeatureSchema), required=True)
