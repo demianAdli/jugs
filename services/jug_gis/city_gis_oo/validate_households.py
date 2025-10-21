3714a8b4b3382c10f4fd68d791deca6715c4e385
Alireza Adli <alireza.adli@mail.concordia.ca>
2025-10-21 12:33:58 -0400

    Allocate none code units

--- services/jug_gis/city_gis_oo/validate_households.py
+++ services/jug_gis/city_gis_oo/validate_households.py
@@ -13,6 +13,15 @@ need to load files in another class like read_geojson_content in jug_ee
 so the wf can work either it is in the middle of the cleaning wf,
 or receiving data through api or receiving just a geojson content/file
 
+I think it makes more sense to have number of units as the base
+for distributing Nones, rather than total_area. Because, it makes more
+sense if we say, an area that has more units should take more of
+Nones (more units) rather than an area which has more total _area.
+
+At the end, assign the methods which depends on a lot of methods
+in the wf class
+
+
 In this module, I refer to the fsa code as code
 """
 from pathlib import Path
@@ -46,7 +55,7 @@ class ValidateHouseholds:
             all_codes.add(self.district.iloc[unit]['CODE_POSTA'][:3])
         return list(all_codes)
 
-    def none_num(self):
+    def none_codes(self):
         total_nones = 0
         for unit in range(self.district_units_num):
             if self.district.iloc[unit]['CODE_POSTA'] == 'None' \
@@ -55,7 +64,7 @@ class ValidateHouseholds:
         nones_percentage = total_nones * 100 / len(self.district)
         return total_nones, nones_percentage
 
-    def summarize_code_area(self, code):
+    def summarize_code_unit_and_area(self, code):
         code_total_area = 0
         code_unit_nums = 0
         for unit in range(self.district_units_num):
@@ -64,25 +73,32 @@ class ValidateHouseholds:
                 code_total_area += self.district.iloc[unit]['total_area']
         return code_unit_nums, code_total_area
 
-    def summarize_area_for_all(self):
+    def summarize_area_and_unit_for_all(self):
         areas_and_units_num = []
         for code in self.district_codes:
-            areas_and_units_num.append(self.summarize_code_area(code))
+            areas_and_units_num.append(self.summarize_code_unit_and_area(code))
         return areas_and_units_num
 
-    def calculate_code_units_frequency_ratio(self):
-        code_frequencies = \
-            [info[0] for info in self.summarize_area_for_all()]
+    def calculate_codes_frequency(self):
+        return [info[0] for info in self.summarize_area_and_unit_for_all()]
+
+    def calculate_codes_unit_frequency_ratio(self):
+        code_frequencies = self.calculate_codes_frequency()
         district_units_num = sum(code_frequencies)
         return [code_frequency * 100 / district_units_num
                 for code_frequency in code_frequencies]
 
-    def calculate_code_area_frequency_ratio(self):
+    def calculate_codes_area_frequency_ratio(self):
         code_area_frequencies = \
-            [info[1] for info in self.summarize_area_for_all()]
+            [info[1] for info in self.summarize_area_and_unit_for_all()]
         district_total_area = sum(code_area_frequencies)
         return [area_frequency * 100 / district_total_area
                 for area_frequency in code_area_frequencies]
 
     def allocate_none_code_units(self):
-        
+        none_nums = self.none_codes()[0]
+        codes_unit_ratio = self.calculate_codes_unit_frequency_ratio()
+        allocated_nones = [round(21 * ratio / 100) for ratio in codes_unit_ratio]
+        code_units = self.calculate_codes_frequency()
+        zip_codes_and_nones = zip(code_units, allocated_nones)
+        return [units for units in zip_codes_and_nones]
