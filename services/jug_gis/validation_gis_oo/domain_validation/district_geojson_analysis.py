aef630c664a3805d205f4b58d4adec1e411ce547
Alireza Adli <alireza.adli@mail.concordia.ca>
2025-10-23 16:52:25 -0400

    Fix & Finalize DistrictGeoJSONAnalysis

--- services/jug_gis/validation_gis_oo/domain_validation/district_geojson_analysis.py
+++ services/jug_gis/validation_gis_oo/domain_validation/district_geojson_analysis.py
@@ -18,41 +18,19 @@ class DistrictGeoJSONAnalysis:
         total_nones = 0
         for unit in range(self.district_units_num):
             if self.district.iloc[unit][postal_code_key] == 'None' \
-                    and self.district.iloc[unit][function_key] == function_value:
+                    and self.district.iloc[unit][function_key] == \
+                    function_value:
                 total_nones += 1
         nones_percentage = total_nones * 100 / len(self.district)
         return total_nones, nones_percentage
 
-    def summarize_code_unit_and_area(self, postal_code_key, area, postal_code_value):
+    def summarize_code_unit_and_area(
+            self, postal_code_key, area, postal_code_value):
         code_total_area = 0
         code_unit_nums = 0
         for unit in range(self.district_units_num):
-            if self.district.iloc[unit][postal_code_key][:3] == postal_code_value:
+            if self.district.iloc[unit][postal_code_key][:3] == \
+                    postal_code_value:
                 code_unit_nums += 1
-                code_total_area += self.district.iloc[unit][area]
+                code_total_area += float(self.district.iloc[unit][area])
         return code_unit_nums, code_total_area
-
-    def calculate_codes_frequency(self):
-        return [info[0] for info in self.summarize_area_and_unit_for_all()]
-
-    def calculate_codes_unit_frequency_ratio(self):
-        code_frequencies = self.calculate_codes_frequency()
-        district_units_num = sum(code_frequencies)
-        return [code_frequency * 100 / district_units_num
-                for code_frequency in code_frequencies]
-
-    def calculate_codes_area_frequency_ratio(self):
-        code_area_frequencies = \
-            [info[1] for info in self.summarize_area_and_unit_for_all()]
-        district_total_area = sum(code_area_frequencies)
-        return [area_frequency * 100 / district_total_area
-                for area_frequency in code_area_frequencies]
-
-    def allocate_none_code_units(self):
-        none_nums = self.none_codes()[0]
-        codes_unit_ratio = self.calculate_codes_unit_frequency_ratio()
-        allocated_nones = [round(none_nums * ratio / 100) for ratio in codes_unit_ratio]
-        code_units = self.calculate_codes_frequency()
-        zip_codes_and_nones = zip(code_units, allocated_nones)
-        return [units for units in zip_codes_and_nones]
-# How can I use the address or other geojson fields to allocate the nones to the actual fsa
--- services/jug_gis/validation_gis_oo/domain_validation/validate_gisoo.py
+++ services/jug_gis/validation_gis_oo/domain_validation/validate_gisoo.py
@@ -27,7 +27,7 @@ class ValidateGISOO:
     self.area_key = area_key
 
     self.district = DistrictGeoJSONAnalysis(self.load_district)
-    self.district_codes = self.district.return_all_codes(self.postal_code_key )
+    self.district_codes = self.district.return_all_codes(self.postal_code_key)
 
   def summarize_area_and_unit_for_all(self):
       areas_and_units_num = []
@@ -36,3 +36,36 @@ class ValidateGISOO:
               self.district.summarize_code_unit_and_area(
                 self.postal_code_key, self.area_key, postal_code_value=code))
       return areas_and_units_num
+
+  def calculate_codes_frequency(self):
+    return [info[0] for info in self.summarize_area_and_unit_for_all()]
+
+  def calculate_codes_unit_frequency_ratio(self):
+    code_frequencies = self.calculate_codes_frequency()
+    district_units_num = sum(code_frequencies)
+    return [code_frequency * 100 / district_units_num
+            for code_frequency in code_frequencies]
+
+  def calculate_codes_area_frequency(self):
+    return [info[0] for info in self.summarize_area_and_unit_for_all()]
+
+  def calculate_codes_area_frequency_ratio(self):
+    code_area_frequencies = self.calculate_codes_area_frequency()
+    district_total_area = sum(code_area_frequencies)
+    return [area_frequency * 100 / district_total_area
+            for area_frequency in code_area_frequencies]
+
+  def allocate_none_codes(self):
+    none_nums = self.district.none_codes(
+      self.postal_code_key, self.function_key, self.function_value)[0]
+    codes_unit_ratio = self.calculate_codes_unit_frequency_ratio()
+    allocated_nones = [
+      round(none_nums * ratio / 100) for ratio in codes_unit_ratio]
+    code_units = self.calculate_codes_frequency()
+    zip_codes_and_nones = zip(code_units, allocated_nones)
+    return [units for units in zip_codes_and_nones]
+
+  def allocate_none_codes_address_base(self):
+    """ How can I use the address or other geojson fields
+    to allocate the nones to the actual fsa"""
+    pass
