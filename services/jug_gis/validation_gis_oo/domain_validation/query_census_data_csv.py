20b8d8906a4a80be6b16aa6bb25cf9c2ba39e880
Alireza Adli <alireza.adli@mail.concordia.ca>
2025-10-23 17:15:35 -0400

    Return the units count

--- services/jug_gis/validation_gis_oo/domain_validation/query_census_data_csv.py
+++ services/jug_gis/validation_gis_oo/domain_validation/query_census_data_csv.py
@@ -1,2 +1,11 @@
 class QueryCensusDataCSV:
-  pass
+  def __init__(self, census_data):
+    self.census_data = census_data
+
+  def census_code_units_num(self, census_code_field_title,
+                            cencus_code_units_num_field_title,
+                            census_code):
+    return self.census_data.loc[
+      self.census_data[
+        census_code_field_title].eq(census_code),
+      cencus_code_units_num_field_title].iloc[0]
