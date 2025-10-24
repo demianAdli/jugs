5c87de22b9cff3b8cfe0538672e4d0584dccaa56
Alireza Adli <alireza.adli@mail.concordia.ca>
2025-10-24 11:34:31 -0400

    Read the number of private dwellings from census data

--- services/jug_gis/validation_gis_oo/domain_validation/query_census_data_csv.py
+++ services/jug_gis/validation_gis_oo/domain_validation/query_census_data_csv.py
@@ -1,11 +1,14 @@
 class QueryCensusDataCSV:
-  def __init__(self, census_data):
-    self.census_data = census_data
+  def __init__(self,
+               census_data,
+               census_code_field_title,
+               cencus_code_units_num_field_title):
 
-  def census_code_units_num(self, census_code_field_title,
-                            cencus_code_units_num_field_title,
-                            census_code):
-    return self.census_data.loc[
-      self.census_data[
-        census_code_field_title].eq(census_code),
-      cencus_code_units_num_field_title].iloc[0]
+    self.lookup = (
+      census_data
+        .set_index(census_code_field_title, drop=True)
+      [cencus_code_units_num_field_title]
+    )
+
+  def census_code_units_num(self, census_code):
+    return self.lookup.get(census_code)
--- services/jug_gis/validation_gis_oo/domain_validation/validate_gisoo.py
+++ services/jug_gis/validation_gis_oo/domain_validation/validate_gisoo.py
@@ -8,6 +8,7 @@ from .district_geojson_analysis import DistrictGeoJSONAnalysis
 
 class ValidateGISOO:
   def __init__(self, census_data_csv, district_data_geojson,
+               census_code_field_title, census_units_num_title,
                postal_code_key, function_key, function_value, area_key):
     base_dir = Path(__file__).resolve().parent.parent
 
@@ -20,7 +21,12 @@ class ValidateGISOO:
       encoding_errors="replace",
       low_memory=False)
 
-    self.census_data = QueryCensusDataCSV(self.load_census_data)
+    self.census_code_field_title = census_code_field_title
+    self.census_units_num_title = census_units_num_title
+
+    self.census_data = QueryCensusDataCSV(self.load_census_data,
+                                          self.census_code_field_title,
+                                          self.census_units_num_title)
 
     self.load_district = gpd.read_file(the_district_path)
 
@@ -40,6 +46,10 @@ class ValidateGISOO:
       function_value=self.function_value
     )
 
+  def census_units_num_all(self):
+    units_num = self.census_data.lookup.reindex(self.district_codes)
+    return units_num.to_dict()
+
   def calculate_codes_unit_frequency_ratio(self):
     district_total_area = sum(
       [index[0] for index in self.all_codes_dict.values()])
@@ -51,3 +61,9 @@ class ValidateGISOO:
       [index[1] for index in self.all_codes_dict.values()])
     return {code: self.all_codes_dict[code][1] * 100 / district_total_area
             for code in self.all_codes_dict.keys()}
+
+  def census_vs_clean_district(self, code):
+    pass
+
+  def census_vs_clean_districts(self):
+    pass
