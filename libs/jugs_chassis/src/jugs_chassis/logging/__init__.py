48d1761154771183a703b32c8a1113e3e828bed4
Alireza Adli <alireza.adli@mail.concordia.ca>
2026-02-11 14:35:01 -0500

    Modularize logging setup and expose public API

--- libs/jugs_chassis/src/jugs_chassis/logging/__init__.py
+++ libs/jugs_chassis/src/jugs_chassis/logging/__init__.py
@@ -1,3 +1,9 @@
-from .setup_logging import configure_logging, set_request_id, get_request_id
+from .context import set_request_id, get_request_id
+from .config import configure_logging, infer_service_name
 
-__all__ = ['configure_logging', 'set_request_id', 'get_request_id']
+__all__ = [
+    'configure_logging',
+    'infer_service_name',
+    'set_request_id',
+    'get_request_id',
+]
