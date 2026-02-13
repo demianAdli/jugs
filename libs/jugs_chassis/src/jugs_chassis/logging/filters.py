ce6482d988844e344d0bb348c38e4f3da3226d9d
Alireza Adli <alireza.adli@mail.concordia.ca>
2026-02-13 10:28:54 -0500

    Add ANSI stripping filter to loging outoput

--- libs/jugs_chassis/src/jugs_chassis/logging/filters.py
+++ libs/jugs_chassis/src/jugs_chassis/logging/filters.py
@@ -2,8 +2,12 @@ from __future__ import annotations
 
 import logging
 import os
+import re
 
 from .context import get_request_id
+
+
+_ANSI_RE = re.compile(r'\x1B\[[0-?]*[ -/]*[@-~]')
 
 
 class ContextFilter(logging.Filter):
@@ -20,4 +24,14 @@ class ContextFilter(logging.Filter):
         record.request_id = get_request_id()
         record.service = self._service or os.getenv('LOG_SERVICE', 'jugs-service')
         record.env = self._env or os.getenv('LOG_ENV', 'dev')
+        return True
+
+
+class StripANSIFilter(logging.Filter):
+    def filter(self, record: logging.LogRecord) -> bool:
+        message = record.getMessage()
+        cleaned = _ANSI_RE.sub('', message)
+
+        record.msg = cleaned
+        record.args = ()
         return True
