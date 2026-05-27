ac595faf5ecdd9a91df22c51452cedb91ac7cdc1
Alireza Adli <alireza.adli@mail.concordia.ca>
2026-05-26 20:21:22 -0400

    Keep the fields

--- services/jug_gis_cities/src/jug_gis_cities/saint_malachie_gisoo/contract_adapter.py
+++ services/jug_gis_cities/src/jug_gis_cities/saint_malachie_gisoo/contract_adapter.py
@@ -21,21 +21,27 @@ workflow_output_layer_path = \
     workflow_output_layer_suffix
 
 output_layer_relative_path = 'saint_malachie_standardized.shp'
-contract_output_layer_path = paths.output_paths_dir + output_layer_relative_path
+output_layer_path = paths.output_paths_dir + output_layer_relative_path
 
 rename_fields = {
-    'heightmax': 'height',
-    'g_utilisat':  'function',
-    'rl_ad_ad_1': 'address',
     'g_id_provi': 'name',
+    'heightmax': 'height',
+    'g_utilisat': 'function',
+    'rl_ad_ad_1': 'address',
     'rl_uerl0_6': 'year_of_construction'
 }
 
 keep_set = rename_fields.keys()
 
-field_schema_manager = FieldSchemaManager(
+saint_malachie_schema_manager = FieldSchemaManager(
     qgis_path=paths.qgis_path,
     layer_path=workflow_output_layer_path,
     layer_name='saint_malachie_gisoo_with_fsa'
 )
+
+standardized_saint_malachie = \
+    saint_malachie_schema_manager.standardize_fields(
+        field_rename_map=rename_fields,
+        fields_to_keep=keep_set
+    )
 
