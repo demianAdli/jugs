5762be9dd775dac82c312e212b2f41c43d671e4d
Alireza Adli <alireza.adli@mail.concordia.ca>
2026-06-29 11:50:24 -0400

    Add new output keys.

---
+++ services/jug_gis_cities/src/jug_gis_cities/mtl_fsa_gisoo/workflow_config.py
@@ -0,0 +1,80 @@
+"""
+Municipality: Saint-Malachie
+workflow_config module
+Project Developer: Alireza Adli 
+alireza.adli@mail.concordia.ca
+alireza.adli4@gmail.com
+www.demianadli.com
+"""
+
+import os
+
+
+default_data_dir = 'D:/GIS/mtl_gisoo_fsa_data'
+data_dir = os.getenv(
+  'JUG_GIS_CITIES_SAINT_MALACHIE_DATA_DIR',
+  default_data_dir)
+input_data_dir = os.path.join(data_dir, 'input_data')
+
+# Application's path
+qgis_path = os.getenv(
+  'JUG_GIS_CITIES_QGIS_PATH',
+  'C:/Program Files/QGIS 3.34.1/apps/qgis')
+
+# Gathering input data layers paths 
+input_paths = {
+  'qc_property_roll_2025':
+  os.path.join(
+    input_data_dir,
+    'mtl_role_2025_unit_eval_et_address_geopackage',
+    'mtl_island_role_2025_unit_eval_et_address.gpkg'),
+  'nrcan_heights':
+  os.path.join(
+    input_data_dir,
+    'mtl_auto_with_heights',
+    'mtl_auto_with_heights.gpkg'),
+  'qc_geoindex':
+  os.path.join(
+    input_data_dir,
+    'mtl_mahm_usage_2026',
+    'mamh_usage_predo_2026_s_poly.shp'),
+  'fsa':
+  os.path.join(
+    input_data_dir,
+    'mtl_island_fsa_boundaries',
+    'mtl_island_fsa_boundaries_dmti_2025.shp')
+}
+
+# Defining a directory for all the output data layers
+output_paths_dir = os.getenv(
+  'JUG_GIS_CITIES_SAINT_MALACHIE_OUTPUT_DIR',
+  os.path.join(data_dir, 'output_data'))
+
+# Preparing a bedding for output data layers paths
+output_paths = {
+  'nrcan_fixed': '',
+  'usage_fixed': '',
+  'roll': '',
+  'usage_margin_sans': '',
+  'usage_san_sans': '',
+  'usage_margins': '',
+  'usage_only': '',
+  'roll_only': '',
+  'usage_roll_only_all': '',
+  'usage_roll_only': '',
+  'usage_roll_only_uniques': '',
+  'roll_clean': '',
+  'usage_roll': '',
+  'usage_roll_all': '',
+  'nrcan_preserved': '',
+  'usage_dup': '',
+  'usage_dup_clean': '',
+  'nrcan_inter': '',
+  'inter_summary': '',
+  'inter_kept': '',
+  'nrcan_restored': '',
+  'nrcan_restored_with_usage_id': '',
+  'nrcan_intersected': '',
+  'nrcan_spatial_join_usage': '',
+  'saint_malachie_gisoo_with_fsa': ''
+}
